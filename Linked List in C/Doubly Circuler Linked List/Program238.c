afc1e6fb48bc1ac3bf5d206e0ca944c355caa1de
Dipak Rasal <133906634+dipakrasal2009@users.noreply.github.com>
2023-12-13 21:21:03 +0530

    Doubly Circuler Linked List using c

---
+++ Linked List in C/Doubly Circuler Linked List/Program238.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)     // 7th January
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+    PNODE Head = NULL;
+    PNODE Tail = NULL;
+
+    return 0;
+}
