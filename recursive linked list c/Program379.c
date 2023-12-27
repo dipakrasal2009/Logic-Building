f69411e1cf12d62344120aa81aa3e221f3c56bbc
Dipak Rasal <133906634+dipakrasal2009@users.noreply.github.com>
2023-12-27 14:56:55 +0530

    recursive linked list in c language

---
+++ recursive linked list c/Program379.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+    PNODE temp = *First;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL)  
+    {
+        *First = newn;
+    }
+    else                
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements from the Linked List are : \n");
+
+    while(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+    }
+    printf("NULL \n");
+}
+
+void DisplayR(PNODE First)
+{
+    if(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+        DisplayR(First);
+    }
+}
+
+void DisplayR1(PNODE First)
+{
+    if(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        DisplayR1(First->next);
+    }
+}
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+int CountR(PNODE First)
+{
+    static int iCnt = 0;
+
+    if(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+        CountR(First);
+    }
+    return iCnt;
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+
+    InsertLast(&Head,11);
+    InsertLast(&Head,21);
+    InsertLast(&Head,51);
+    InsertLast(&Head,101);
+
+    DisplayR1(Head);
+
+    iRet = CountR(Head);
+    printf("\nNumber of nodes are : %d\n",iRet);
+
+    return 0;
+}
